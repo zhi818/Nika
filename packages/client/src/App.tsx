6a63aabd62662596b2bbe21def0c75c05925f32a
lizhi <932059909@qq.com>
2025-04-30 01:07:00 +0800

    chore: monorepo结构 包含后端 前端 客户端 小程序

---
+++ packages/client/src/App.tsx
@@ -0,0 +1,8 @@
+/*
+ * @Author: lizhi (zhi.li@geekplus.com)
+ * @Date: 2025-04-30 00:48:29
+ */
+import { createRoot } from 'react-dom/client';
+
+const root = createRoot(document.body);
+root.render(<h2>Hello from React!</h2>);
