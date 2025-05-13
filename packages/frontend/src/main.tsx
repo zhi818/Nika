8a05ef053e703bdfab305dc68bab34c1cb4e384a
zhi818 <zli_818@outlook.com>
2025-05-14 02:28:17 +0800

    feat: 通过React Router管理路由 并且新增播放器页面

--- packages/frontend/src/main.tsx
+++ packages/frontend/src/main.tsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { BrowserRouter, Routes, Route } from 'react-router'
+
 import App from './App.tsx'
+import PlayerPage from './pages/Player'
+
+import './index.css'
 
 createRoot(document.body).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/player" element={<PlayerPage />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
---
+++ packages/frontend/src/pages/Player/index.tsx
@@ -0,0 +1,10 @@
+function Player() {
+  return (
+    <div>
+      <h1>Player</h1>
+      <p>Player page content goes here.</p>
+    </div>
+  )
+}
+
+export default Player
